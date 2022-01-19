329f457a0e7f5d7ab1da8bcd7d0ac213f2679cee
Antonio Tamez Salinas <Antonio.Tamez@chubb.com>
2022-01-18 18:49:34 -0600

    creando interfaz de respuestas

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RespuestaMDB } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class MoviesService {
   constructor(private http: HttpClient) { }
 
   getFeature(){
-    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2022-01-01&primary_release_date.lte=2022-02-01&api_key=feb24f7bf104963a87d169004a10db25&language=es&include_image_language=es`);
+    return this.http.get<RespuestaMDB>(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2022-01-01&primary_release_date.lte=2022-02-01&api_key=feb24f7bf104963a87d169004a10db25&language=es&include_image_language=es`);
   
   }
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { Pelicula } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -8,11 +9,16 @@ import { MoviesService } from '../services/movies.service';
 })
 export class Tab1Page implements OnInit {
 
+ peliculasRecientes: Pelicula[] = [];
+
   constructor(private moviesService: MoviesService) {}
 
 
   ngOnInit() {
-    this.moviesService.getFeature().subscribe( console.log );
+    this.moviesService.getFeature().subscribe( resp => {
+      console.log('Resp',resp);
+      this.peliculasRecientes = resp.results;
+    } );
   }
 
 }
