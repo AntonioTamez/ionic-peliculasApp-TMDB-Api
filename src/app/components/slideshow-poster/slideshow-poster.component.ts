8cf5dde4a01a7a22d08dddde3afd0b14f329e9a4
Antonio Tamez Salinas <Antonio.Tamez@chubb.com>
2022-03-18 07:30:31 -0600

    creacion de apk

--- src/app/components/slideshow-poster/slideshow-poster.component.ts
+++ src/app/components/slideshow-poster/slideshow-poster.component.ts
@@ -15,23 +15,23 @@ export class SlideshowPosterComponent implements OnInit {
   slideOpts = {
     slidesPerView: 3.3, 
     freeMode: true,  
-    // breakpoints: {
-    //   // when window width is >= 320px
-    //   320: {
-    //     slidesPerView: 2,
-    //     spaceBetween: 20
-    //   },
-    //   // when window width is >= 480px
-    //   480: {
-    //     slidesPerView: 3,
-    //     spaceBetween: 30
-    //   },
-    //   // when window width is >= 640px
-    //   640: {
-    //     slidesPerView: 4,
-    //     spaceBetween: 40
-    //   }
-    // }
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 3.5,
+        spaceBetween: 10
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 5.5,
+        spaceBetween: 20
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 7.5,
+        spaceBetween: 30
+      }
+    }
   };
 
   constructor(private modalCtrl : ModalController) { }
