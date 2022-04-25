4fc647603907f5ea5b253ab2891998f650a63803
Antonio Tamez Salinas <Antonio.Tamez@chubb.com>
2022-04-25 07:51:30 -0500

    capacitor added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'peliculasApp',
+  appId: 'io.ionic.peliculas',
+  appName: 'peliculas',
   webDir: 'www',
   bundledWebRuntime: false
 };
